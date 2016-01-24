986a67eecfaa32819ccddbb304595ca831060b57
Howard Ge <howard.ge@sap.com>
2016-01-24 14:14:00 +0800

    没有 clean up
     - 先创建一个Mock的实例，传入调用者，stub_method，stub_args存为实例变量，返回Mock实例
     - returns 返回调用者
     - 如果调用者是一个实例，则不创建module
     - 如果是一个Class，则创建一个module，在这个module 中创建stub_method方法，并prepend进调用者

---
+++ mock.rb
@@ -0,0 +1,49 @@
+class Mock
+
+  def initialize(stubee)
+    @the_stubee = stubee
+  end
+
+  def with(*arg)
+    @stub_args = arg
+    self
+  end
+
+  def returns(value)
+    stub_args   = @stub_args
+    stub_method = @stub_method
+    l = lambda { |*args| (stub_args && stub_args != args) ? (raise) : (return value) }
+
+    if @all_instances
+      modu = Module.new
+      modu.send(:define_method, stub_method, l)
+      @the_stubee.class_eval { prepend modu }
+    else
+      if @the_stubee.is_a?(Class)
+        modu = Module.new
+        modu.send(:define_method, stub_method, l)
+        @the_stubee.singleton_class.class_eval { prepend modu }
+      else
+        @the_stubee.define_singleton_method(stub_method, l)
+      end
+    end
+  end
+end
+
+
+class Object
+  def stubs(method_name)
+    target = self.is_a?(Mock) ? self : Mock.new(self)
+    target.instance_eval { @stub_method = method_name.to_sym }
+    target
+  end
+
+  def all_instances
+    mock = Mock.new(self)
+    mock.instance_eval { @all_instances = true }
+    mock
+  end
+end
+
+
+
